114cef5b17bc8fb52557fcc76e35462c092ba637
Rohan Maharjan <maharjanrohan16@gmail.com>
2020-08-28 07:35:12 +0545

    new c files

---
+++ ex1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+//h= header file
+//stdio=standard input output
+
+#include<conio.h>
+// console = black screen
+
+void main()
+{
+	//instruction
+	printf("Hello Rohan");
+	// ""= terminate
+	// printf=
+	getch(); // to hold the screen	
+}
