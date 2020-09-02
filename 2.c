fe7e52e8c6302c54496e8ada97d9ae5dfd0b1021
Rohan Maharjan <maharjanrohan16@gmail.com>
2020-09-02 13:00:11 +0545

    added new files

---
+++ 2.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+void main(){
+	// data and data types
+	// integer
+	/*
+	int// data type
+	num1 // identifiers 
+	;// this together is called variable declaration
+	num1=5;// varaiable initialization
+	*/
+	
+	// in one step it is called variable declaration and initization
+//	int num1=5;
+//	printf("A number is %d",num1);
+	// it print num 1
+	// ("") = conversion specifier
+	
+	//float num1 =5.5;
+	//printf("A number is %.2f",num1); // .2 gives only two decimal values.
+	
+	//double num1=5.5;
+	//printf("A number is %f",num1);
+	
+	//char num1='A';// should use '' in char 
+	//printf("A number is %d",num1);//%c gives the output a and %d gives ascii number
+	
+	int a;
+	char b;
+	float c;
+	double d;
+	printf("Storage size for int data type:%d\n",sizeof(a));
+	printf("Storage size for char data type:%d\n",sizeof(b));
+	printf("Storage size for float data type:%d\n",sizeof(c));
+	printf("Storage size for double data type:%d\n",sizeof(d));
+	
+	
+	
+	//comment=helps to make program readable or ignore 
+	/* 
+	for 
+	multiple
+	comment 
+	*/
+	
+	
+	
+	
+}
