4f4e839affc1131c22efc4470439934a21be7df5
rojina maharjan <maharjanrojina16@gmail.com>
2020-09-10 08:29:56 +0545

    added file

---
+++ question15.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	float meter,cm;
+	printf("Enter your number in meter:");
+	scanf("%f",&meter);
+	
+	cm=meter*100;
+	printf("The number in cm is %.2fcm",cm);
+	return 0;
+	
+	
+	
+	
+}
