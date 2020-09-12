23f8dcd4b82686eb2414960183b843e2ea70ae66
Rohan Maharjan <maharjanrohan16@gmail.com>
2020-09-12 21:52:59 +0545

    modified 23

--- question23.c
+++ question23.c
@@ -4,14 +4,14 @@ void main()
 {
 	
 	int salary,tax,all,net;
-	printf("Enter your basic salary:");
+	printf("Enter your basic salary:Rs");
 	scanf("%d",&salary);
 	
 	tax=0.2*salary;
-	printf("The tax amount of salary is %d",tax);
+	printf("The tax amount of salary is Rs%d.",tax);
     
     all=0.4*salary;
-    printf("\nThe allowance amount of salary is %d",all);
+    printf("\nThe allowance amount of salary is Rs%d.",all);
     
     net=salary-tax-all;
     printf("\nThe net salary is %d",net);
