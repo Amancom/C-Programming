d5eeb1ccaf39b67dd62cde60f245d85aea6b3013
Rohan Maharjan <maharjanrohan16@gmail.com>
2020-09-24 16:25:24 +0545

    added

---
+++ Class Practice/eg1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a =19;
+float b =18.5;
+printf("\n%d",a);
+printf("\n%d",12);
+printf("\n%10d",12);
+printf("\n%010d",12);
+int m=1256;
+printf("\n%d",m);
+printf("\n%o",m);
+printf("\n%x",m);
+printf("\n%-5d",m);
+
+printf("\n%.3f",b);
+printf("\n%7.2f",b);
+printf("\n%09.2f",b);
+return 0;
+}
+
---
+++ Class Practice/eg2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+a= printf("hello");
+printf("\n value return from printf is %d",a);
+a= printf("\nhello World");
+printf("\n value return from printf is %d",a);
+
+
+
+return 0;
+}
+
