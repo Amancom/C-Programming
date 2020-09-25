285c16de4e32334878e71aa3c4a4a7905436d0ff
Rohan Maharjan <maharjanrohan16@gmail.com>
2020-09-25 09:59:34 +0545

    added

---
+++ Class Practice/continue.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+int a=0;
+while(a<10)
+{
+	a++;
+	if(a==5)
+	{
+	continue;
+	}
+	printf("\n Hello %d",a);
+	}
+	printf("\n End of Program.");
+}
+
+
+
+
