50c18f0b1e6442ed64327cec1b9f9dcaa49e40ab
Rohan Maharjan <maharjanrohan16@gmail.com>
2020-09-30 13:55:04 +0545

    added

--- lab assigment 2/ques8.c
+++ lab assigment 2/ques8.c
@@ -2,21 +2,25 @@
 #include<conio.h>
 void main()
 {
-int a,b,c;
+int a,b,c,g;
 printf("Enter three numbers:\n");
 scanf("%d%d%d",&a,&b,&c);
 if(a>b&&a>c)
 { 
-printf("%d is greatest.",a);
+   //printf("\n%d is greatest.",a);
+   g=a;
 }
 else if(b>a && b>c)
 {
-	printf("%d is greatest",b);
+	//printf("\n%d is greatest",b);
+	g=b;
 }
 else
 {
-	printf("%d is greatest.",c);
+	//printf("\n%d is greatest.",c);
+	g=c;
 }
+	printf("\n%d is greatest.",g);
 return 0;
 }
 
--- lab assigment 2/ques9.c
+++ lab assigment 2/ques9.c
@@ -2,21 +2,25 @@
 #include<conio.h>
 void main()
 {
-int a,b,c;
+int a,b,c,s;
 printf("Enter three numbers:\n");
 scanf("%d%d%d",&a,&b,&c);
 if(a<b && a<c)
 {
-	printf("%d is smallest.",a);
+	//printf("%d is smallest.",a);
+	s=a;
 }
 else if(b<a && b<c)
 {
-	printf("%d is smallest.",b);
+	//printf("%d is smallest.",b);
+	s=b;
 }
 else
 {
-	printf("%d is smallest",c);
+	//printf("%d is smallest",c);
+	s=c;
 }
+printf("%d is smallest.",s);
 
 
 
