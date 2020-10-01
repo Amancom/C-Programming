741cdc87c40669619dcce1dc59ecded0a211ce9c
Rohan Maharjan <maharjanrohan16@gmail.com>
2020-10-01 14:02:12 +0545

    added

---
+++ lab assigment 3/qu9.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("Enter your electricity bill.");
+scanf("%d",&a);
+if (a>=0 && a<=150,a*3)
+{
+	printf("Your electricity bill is %d",a);
+}
+else if (a>=151 && a<=350)
+{
+	
+}
+
+
+
+return 0;
+}
+
