4bc3139129fe4356865608ccadc702c65bf8ac79
Rohan Maharjan <maharjanrohan16@gmail.com>
2020-10-02 09:27:51 +0545

    added

--- lab assigment 3/qu9.c
+++ lab assigment 3/qu9.c
@@ -5,13 +5,13 @@ void main()
 int a;
 printf("Enter your electricity bill.");
 scanf("%d",&a);
-if (a>=0 && a<=150,a*3)
+if (a>=0 && a<=150)
 {
-	printf("Your electricity bill is %d",a);
+	printf("Your electricity bill is %d",a*3);
 }
 else if (a>=151 && a<=350)
 {
-	
+	printf("Your electricity bill is %d",100+a*3.75)
 }
 
 
